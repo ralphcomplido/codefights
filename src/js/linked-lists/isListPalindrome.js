fa7108b8b618fc4330e373f8bd129834ee1b5de5
wesokuhara <wesokuhara@gmail.com>
2017-09-20 14:56:09 -0700

    completed isListPalindrome()

---
+++ src/js/linked-lists/isListPalindrome.js
@@ -0,0 +1,43 @@
+function ListNode(x) {
+  this.value = x;
+  this.next = null;
+};
+
+function isListPalindrome(l) {
+  // empty list or list of length 1
+  if (l === null || l.next === null) {
+    return true;
+  }
+
+  const stack = [];
+  let slow = l;
+  let fast = l;
+
+  // use slow, fast pointer method
+  // when fast reaches the end, slow will be halfway
+  while (fast && fast.next) {
+    stack.push(slow.value);
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  // for odd length lists
+  if (fast) {
+    slow = slow.next;
+  }
+
+  // verify the next values match the stack values
+  while (stack.length !== 0) {
+    if (stack.pop() !== slow.value) {
+      return false;
+    }
+    slow = slow.next;
+  }
+
+  return true;
+};
+
+module.exports = {
+  ListNode,
+  isListPalindrome
+};
